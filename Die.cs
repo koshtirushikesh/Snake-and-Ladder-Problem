74319d2084a14bbe67592f6912744d81413c86b6
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2022-12-20 17:29:59 +0530

    The Player rolls the die

---
+++ Die.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class Die
+    {
+        public static void RollDie()
+        {
+            int PlayerPossition = 0;
+
+            Random random= new Random();
+            int Roll_die = random.Next(1,7);
+
+            Console.WriteLine(Roll_die);
+        }
+    }
+}
