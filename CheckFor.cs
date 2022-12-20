ea16fd842c653eb0c80576c7b9053119414ffe64
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2022-12-20 17:34:04 +0530

    checks for a Option. They are No Play, Ladder or Snake.

---
+++ CheckFor.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class CheckFor
+    {
+        public static void SnakeAndLadder()
+        {
+            int PlayerPossition = 0;
+
+            Random random = new Random();
+
+            int Roll_die = random.Next(1, 7);
+            int Option = random.Next(3);
+
+            Console.WriteLine(Roll_die);
+            PlayerPossition += Roll_die;
+            Console.WriteLine(PlayerPossition);
+
+            if (Option == 0)
+            {  
+                PlayerPossition = PlayerPossition - Roll_die;
+                Console.WriteLine("snake "+ PlayerPossition);
+            }
+            else if (Option == 1)
+            {
+                PlayerPossition = PlayerPossition + Roll_die;
+                Console.WriteLine("ladder "+ PlayerPossition);
+            }
+            else
+            {
+                Console.WriteLine("no play "+ PlayerPossition);
+            }
+
+
+        }
+    }
+}
