6ca28f884546801c1f80ffefa5f5e997f2ccc324
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2022-12-20 17:26:13 +0530

    Snake and Ladder game played with single player at start position 0

---
+++ SnakeAndLader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class SnakeAndLader
+    {
+        public static void possition()
+        {
+            int PlayerPossition = 0;
+            Console.WriteLine("The Player has started from 0 possition");
+        }
+    }
+}
