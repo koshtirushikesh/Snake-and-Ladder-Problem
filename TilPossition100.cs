99f2300d2668adfdbf207199dca6c96ebab81f87
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2022-12-20 17:38:23 +0530

    till the Player reaches the winning position 100.

---
+++ TilPossition100.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class TilPossition100
+    {
+        public static void TillThe100()
+        {
+            int PlayerPossition = 0;
+
+            Random random = new Random();
+            
+            while (PlayerPossition < 100)
+            {
+                int Roll_die = random.Next(1, 7);
+                int Option = random.Next(3);
+
+                PlayerPossition += Roll_die;
+
+                if (Option == 0)
+                {
+                    PlayerPossition = PlayerPossition - Roll_die;
+                    Console.WriteLine("snake " + PlayerPossition);
+                }
+                else if (Option == 1)
+                {
+                    PlayerPossition = PlayerPossition + Roll_die;
+                    Console.WriteLine("ladder " + PlayerPossition);
+                }
+                else
+                {
+                    Console.WriteLine("no play " + PlayerPossition);
+                }
+
+                if (PlayerPossition > 100) PlayerPossition -= Roll_die;
+                if (PlayerPossition < 0) PlayerPossition = 0;
+                if (PlayerPossition == 100) Console.WriteLine("player has won");
+                
+            }
+
+        }
+    }
+}
