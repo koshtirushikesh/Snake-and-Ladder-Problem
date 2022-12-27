99eebfed70a942928a816492207c03db678eea16
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2022-12-27 15:43:12 +0530

    Adding player 2

---
+++ Option.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class Option
+    {
+        public static int mPlayerPossition(int PlayerPossition)
+        {
+            Random random = new Random();
+            int Roll_die = random.Next(1, 7);
+            int Option = random.Next(3);
+
+            PlayerPossition += Roll_die;
+
+            if (Option == 0)
+            {
+                PlayerPossition = PlayerPossition - Roll_die;
+                Console.WriteLine("snake " + PlayerPossition);
+            }
+            else if (Option == 1)
+            {
+                PlayerPossition = PlayerPossition + Roll_die;
+                Console.WriteLine("ladder " + PlayerPossition);
+            }
+            else
+            {
+                Console.WriteLine("no play " + PlayerPossition);
+            }
+            if (PlayerPossition > 100) PlayerPossition -= Roll_die;
+            if (PlayerPossition < 0) PlayerPossition = 0;
+
+            return PlayerPossition;
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Snake & Ladder Simulator");
+
+            int PlayerPossition_1 = 0, PlayerPossition_2 = 0;
+
+
+            Console.WriteLine("The Player has started from 0 possition");
+            
+            while(true)
+            {
+                 PlayerPossition_1 = Option.mPlayerPossition(PlayerPossition_1);
+                Console.WriteLine("player 1 possition " + PlayerPossition_1);
+
+                 PlayerPossition_2 = Option.mPlayerPossition(PlayerPossition_2);
+                Console.WriteLine("player 2 possition " + PlayerPossition_2);
+                if (PlayerPossition_1 == 100)
+                {
+                    Console.WriteLine("player 1 has won ");
+                    break;
+                }
+                else if (PlayerPossition_2 == 100)
+                {
+                    Console.WriteLine("player 2 has won ");
+                    break;
+                }
+            }
+
+            
+        }
+    }
+}
